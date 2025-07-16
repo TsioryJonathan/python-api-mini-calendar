cc357d9ef15459acf10421b07b7357b2baa425e4
Jonathan <rakotoarisoajonathan0@gmail.com>
2025-07-16 19:41:34 +0300

    feat: /events POST endpoint

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import json
 from fastapi import FastAPI, Request
 from starlette.responses import Response
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from typing import List
+
 app = FastAPI()
 @app.get("/")
 def root(request: Request):
@@ -29,13 +32,42 @@ def root(request: Request):
         media_type="text/html"
     )
 
+class EventModel(BaseModel):
+    name: str
+    description: str
+    start_date: str
+    end_date: str
+
+events_store: List[EventModel] = [EventModel(
+    name="Hackathon 2025",
+    description="Concours de programmation",
+    start_date="2025-08-01",
+    end_date="2025-08-02"
+)]
+
+
+def serialized_stored_events():
+    events_converted = []
+    for event in events_store:
+        events_converted.append(event.model_dump())
+    return events_converted
+
+@app.get("/events")
+def list_events():
+    return {"events": serialized_stored_events()}
+
+@app.post("/events")
+def create_event(events: List[EventModel]):
+    for event in events:
+        events_store.append(event)
+    return {"Events": serialized_stored_events()}
 @app.get("/{full_path:path}")
 def catch_all(full_path: str):
     with open ("404.html" , "r" , encoding="utf-8") as file:
         html_content = file.read()
-        return Response(
+
+    return Response(
         content=html_content,
         status_code=404,
         media_type="text/html"
-        )
-    return None
+    )
